5f7a6f01052dcbb24a8f268979f96e8f85bf1d52
777lazy <pedro@DESKTOP-TCLLUPA.localdomain>
2026-06-12 20:00:06 -0300

    README

--- src/api/app.js
+++ src/api/app.js
@@ -1,28 +1,23 @@
-require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// CORS para liberar requests do frontend via nginx
-app.use(cors({
-  origin: ['http://localhost', 'http://127.0.0.1', 'http://localhost:80', 'http://127.0.0.1:80', 'http://localhost:5173', 'http://127.0.0.1:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -34,14 +29,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  const status = err.status || 500;
-  return res.status(status).json({
-    success: false,
-    message: err.message || 'Erro interno do servidor',
-    errors: []
-  });
-});
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//Cadastrar Usuário
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET do index - teste da API */
+/* GET home page. */
 router.get('/', function(req, res, next) {
- return res.status(200).json({
-        success: true,
-        message: 'Olá, mundo! Panetone <3'
-      });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
